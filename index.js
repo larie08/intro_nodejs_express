1d7c220ce48c862115732cdf3a6ef0ee0783dbd8
unknown <YY442@ccs.local>
2025-02-07 04:01:11 -0800

    Updated README.md adding my full name

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use((req, res, next)=>{
+    console.log(`${req.method} ${req.url}`);
+});
+
+app.use((err, req, res, next)=>{
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+app.get('/', (req, res)=>{
+    res.send('Hello, World!');
+});
+
+app.get('/about', (req, res)=>{
+    res.send('About Us');
+});
+
+app.post('/submit', (req, res)=>{
+    const data = req.body;
+    res.send(`Received: ${JSON.stringify(data)}`);
+});
+
+app.listen(port, ()=>{
+    console.log(`Server running at http://localhost:${port}`);
+});
+
