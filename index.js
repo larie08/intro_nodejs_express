172b25961bc2febe7aa7e458bca70db48e9fd038
unknown <YY442@ccs.local>
2025-02-07 05:18:30 -0800

    Completed Project

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const port = 3000;
+const items = ['Apple', 'Banana', 'Orange'];
 
 app.use(express.static('public'));
-app.use(express.json());
+app.use(express.json()); //Middleware
+
 app.use((req, res, next)=>{
     console.log(`${req.method} ${req.url}`);
+    next();
 });
 
-app.use((err, req, res, next)=>{
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
+app.post('/submit', (req, res)=>{
+    const data = req.body;
+    res.send(`Received: ${JSON.stringify(data)}`);
 });
 
 app.get('/', (req, res)=>{
@@ -21,12 +24,17 @@ app.get('/about', (req, res)=>{
     res.send('About Us');
 });
 
-app.post('/submit', (req, res)=>{
-    const data = req.body;
-    res.send(`Received: ${JSON.stringify(data)}`);
+app.get('/items', (req, res)=>{
+    res.json(items);
+});
+
+app.post('/items', (req, res)=>{
+    const newItem = req.body.item;
+    items.push(newItem);
+    res.json(items);
 });
 
 app.listen(port, ()=>{
-    console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server is running at http://localhost:${port}`);
 });
 
